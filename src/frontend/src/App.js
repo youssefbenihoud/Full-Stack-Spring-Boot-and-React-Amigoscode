cd388debc4265c4d0962652476122418f24f8f89
benihoud <benihoud@th-brandenburg.de>
2022-10-04 22:09:18 +0200

    add Footer

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -10,7 +10,7 @@ import {
     Button,
     Badge,
     Avatar,
-    Radio
+    Radio, Divider
 } from 'antd';
 import {
     DesktopOutlined,
@@ -209,7 +209,15 @@ function App() {
                     {renderStudents()}
                 </div>
             </Content>
-            <Footer style={{textAlign: 'center'}}>Youssef Benihoud with Amigoscode Course</Footer>
+            <Footer style={{textAlign: 'center'}}>Youssef Benihoud with Amigoscode Course
+            <Divider>
+                <a
+                    target={"_blank"}
+                    href={"https://www.t.me/youssefbenihoud"}>
+                    Click here to contact me on Telegram!
+                </a>
+            </Divider>
+            </Footer>
         </Layout>
     </Layout>
 }
